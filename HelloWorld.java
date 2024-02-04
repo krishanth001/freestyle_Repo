b7f8a63e1808bf48b675e7fa2d395c72a21e6956
krishanth001 <65499174+krishanth001@users.noreply.github.com>
2024-02-04 15:21:47 +0000

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,7 @@
 public class HelloWorld{
       public static void main(String args[]){
           System.out.println("Hello World");
-            System.out.printf("%d + %d = %d", 5, 2).println();
-      }
+            System.out.println("i am Krishanth");      
 }
 
 
